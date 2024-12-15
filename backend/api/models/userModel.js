941632dae06708385c8532e6a620b39f9fe70b77
Tanha <nusrattanha897@gmail.com>
2024-12-16 02:53:27 +0600

    user model is created

---
+++ backend/api/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  studentId: { type: String, required: true, unique: true },
+  gender: { type: String },
+  semester: { type: String },
+  batch: { type: String },
+  registeredForProject: {type : Boolean},
+  proposalAccepted: {type: Boolean},
+  supervisor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  password: { type: String, required: true },
+  confirmPassword:{type: String},
+  role: { type: String, enum: ['admin', 'supervisor', 'student'], required: true },
+  studentStatus: { type: String, enum: ["pending", "approved","declined"], default: "pending" },
+  },
+  {timestamps: true}
+);
+module.exports = mongoose.model('User', userSchema);
